9bac8ad74a954945f4917a5aa25c5dfb903b7c48
aurelien-loyer <aurelien.loyer@epitech.eu>
2026-06-05 11:23:19 +0400

    feat: add legacy support with additional polyfills in Vite configuration

--- site-kiosk/vite.config.ts
+++ site-kiosk/vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["chrome >= 70", "firefox >= 63", "safari >= 12"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
+  build: {
+    target: "es2015",
+  },
 });
